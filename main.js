f87b2cb206b28f0145f617231756537c652ec668
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2021-12-23 20:56:20 +0100

    Create page home and contact

---
+++ main.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById("hamburger")
+const navUL = document.getElementById("links")
+console.log(hamburger)
+
+hamburger.addEventListener('click',()=>{
+    navUL.classList.toggle('show')
+console.log("hi");
+    
+})
